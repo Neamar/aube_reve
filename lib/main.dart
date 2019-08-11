e420c140b6aacafd4c9c5bbd7a9d7de0e8bafd44
Neamar <neamar@neamar.fr>
2019-08-11 18:21:34 +0200

    Main scaffolding for real app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -6,77 +5,54 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final wordPair = WordPair.random();
-
     return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: RandomWords(),
+      title: 'Aube Rêve',
+      home: DiceRoller(),
     );
   }
 }
 
-class RandomWordsState extends State<RandomWords> {
-  final _suggestions = <WordPair>[];
-  final Set<WordPair> _saved = Set<WordPair>();
-  final _biggerFont = const TextStyle(fontSize: 18.0);
-
-  Widget _buildSuggestions() {
-    return ListView.builder(
-        padding: const EdgeInsets.all(16.0),
-        itemBuilder: /*1*/ (context, i) {
-          if (i.isOdd) return Divider();
-          /*2*/
-
-          final index = i ~/ 2; /*3*/
-          if (index >= _suggestions.length) {
-            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
-          }
-          return _buildRow(_suggestions[index]);
-        });
-  }
-
-  Widget _buildRow(WordPair pair) {
-    final bool alreadySaved = _saved.contains(pair);
-    return ListTile(
-      title: Text(
-        pair.asPascalCase,
-        style: _biggerFont,
-      ),
-      trailing: Icon(
-        // Add the lines from here...
-        alreadySaved ? Icons.favorite : Icons.favorite_border,
-        color: alreadySaved ? Colors.red : null,
-      ),
-      onTap: () {
-        setState(() {
-          if (alreadySaved) {
-            _saved.remove(pair);
-          } else {
-            _saved.add(pair);
-          }
-        });
-      },
-    );
-  }
-
-  void _pushSaved() {
-  }
+class DiceRollerState extends State<DiceRoller> {
+  final _diceCount = 5;
+  final _successThreshold = 4;
+  final _currentResult = -1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Startup Name Generator'),
-        actions: <Widget>[
-          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
-        ],
+        title: Text("Lancer de dés Aube Rêve"),
       ),
-      body: _buildSuggestions(),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Column(children: <Widget>[
+              Text('Caractéristique'),
+              Text(
+                '$_diceCount',
+                style: Theme.of(context).textTheme.display2,
+              ),
+            ]),
+            Column(children: <Widget>[
+              Text('Compétence'),
+              Text(
+                '$_successThreshold',
+                style: Theme.of(context).textTheme.display2,
+              ),
+            ]),
+            Text(
+              '$_currentResult',
+              style: Theme.of(context).textTheme.display4,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
 
-class RandomWords extends StatefulWidget {
+class DiceRoller extends StatefulWidget {
   @override
-  RandomWordsState createState() => RandomWordsState();
+  DiceRollerState createState() => DiceRollerState();
 }
